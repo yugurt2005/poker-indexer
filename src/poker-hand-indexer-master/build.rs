403baefa301ec2fbcf74ba76df602ae6691ab267
Chien-Yu Xiong <58307327+yugurt2005@users.noreply.github.com>
2024-08-04 16:01:19 -0700

    clean

--- src/poker-hand-indexer-master/build.rs
+++
@@ -1,9 +0,0 @@
-extern crate cc;
-
-fn main() {
-    cc::Build::new()
-        .file("src_c/hand_index.c")
-        .file("src_c/rust_interface.c")
-        .include("src_c")
-        .compile("hand_index");
-}
